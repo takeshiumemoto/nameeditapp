676a4be6f4c2119d10cdc55bae8b4b7af0de827c
EC2 Default User <ec2-user@ip-172-31-45-89.ap-northeast-1.compute.internal>
2024-07-25 10:42:56 +0000

    react

---
+++ src/components/providers/AdminFlagProvider.jsx
@@ -0,0 +1,17 @@
+import {createContext} from "react";
+
+export const AdminFlagContext = createContext({});
+
+export const AdminFlagProvider = props=>{
+    const{children}=props;
+    
+    const {isAdmin,setIsAdmin} = useState(false);
+    
+    const sampleObj = {sampleValue:"テスト"};
+    
+    return(
+        <AdminFlagContext.Provider value={{isAdmin,setIsAdmin}}>
+            {children}
+        </AdminFlagContext.Provider>  
+        );
+};
